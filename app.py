8670987e61c470d26c29080931894b3980fb3873
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-11-24 16:32:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode="threading")
 
 @app.route('/')
 def index():
@@ -11,10 +11,12 @@ def index():
 @socketio.on('execute_code')
 def execute_code(data):
     try:
+        # Execute the Python code
         result = str(eval(data['code']))
     except Exception as e:
         result = str(e)
+    # Send the result back to the client
     socketio.emit('execution_result', {'result': result})
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=8080)
+    socketio.run(app, debug=True, port=5000)
